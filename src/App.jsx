12946e8b40f8a3db57bfbf6c79dc899c00a2ab6d
AllenY687 <a279yang@uwaterloo.ca>
2025-02-28 20:41:52 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>RSA Algorithm Simulation</h1>
+      <p>Characters: Alice, Bob, and Trudy</p>
+    </div>
+  );
+}
+
+export default App;
